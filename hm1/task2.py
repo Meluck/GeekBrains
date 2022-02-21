1393cd728e39cbf96f636fb68f93e02e76a77692
Марк Лустгартен <40274708+Meluck@users.noreply.github.com>
2022-02-21 21:46:36 +0300

    refactor

--- hm1/task2.py
+++ hm1/task2.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
 
     total_time_sec = int(input('Enter time counted in seconds:\n'))
 
-    SEC_IN_HOUR = 60*60
-    MIN_IN_HOUR = 60
+    sec_in_hour = 60*60
+    min_in_hour = 60
 
     hours = total_time_sec // SEC_IN_HOUR
     minutes = (total_time_sec % SEC_IN_HOUR) // MIN_IN_HOUR
