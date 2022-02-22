cddcc72b33e8d46641cb8f7a333080434fffc031
Mark Lustgarten <marklustgarten@yandex.ru>
2022-02-22 22:00:29 +0300

    добавил дополнительные try-except для 4го задания

--- hm3/task1.py
+++ hm3/task1.py
@@ -14,9 +14,12 @@ def div(arg1, arg2):
     except ZeroDivisionError as zde:
         print(f'div: {zde}')
 
+try:
+    num1, *check_num, num2 = [float(sym) for sym in input('Введите 2 числа через пробел\n').split()]
+    if check_num:
+        print('Input error')
+    else:
+        print(f'Division result: {div(num1, num2)}')
+except Exception as ex:
+    print(ex)
 
-num1, *check_num, num2 = [float(sym) for sym in input('Введите 2 числа через пробел\n').split()]
-if check_num:
-    print('Input error')
-else:
-    print(f'Division result: {div(num1, num2)}')
