a2c77d77b90ca601973effb171802736556d6fad
Mark Lustgarten <marklustgarten@yandex.ru>
2022-02-27 20:14:37 +0300

    поправил ошибку аргументов для второй части и косметические исправления вывода

--- hm4/task6.py
+++ hm4/task6.py
@@ -13,55 +13,52 @@
 
 from sys import argv
 from itertools import count, cycle
-import argparse
 
 help_mes = f'This program has 2 modes:\n' \
            f'int: integer continues numbers generation\n' \
            f'rep: repeat of input sequence while number of' \
            f' elements less then n (20 default)\n' \
            f'Usage example:\n' \
-           f'1) int 10, 20\n' \
+           f'1) int 10 20\n' \
            f'2) rep 1,2,3,4,5 10'
-
-parser = argparse.ArgumentParser(description=help_mes)
-parser.add_argument('int', nargs='*', help=help_mes)
-parser.add_argument('rep', nargs='*')
 
 
 def int_gen_iter(start_num: int, stop_num: int):
     for num in count(start_num):
-        print(f'{num}', end=', ')
-        if num >= stop_num:
+        if num > stop_num:
             break
+        print(f'{num}', end=' ')
 
 
-def repeat_gen_iter(sequence: list, **args):
+def repeat_gen_iter(sequence: list, *args):
     arg, *tail = args
     stop_num = arg if arg is not None else 20
     for idx, el in enumerate(cycle(sequence)):
-        print(el, end=', ')
+        print(el, end=' ')
         if idx >= stop_num - 1:
             break
 
 
 if __name__ == "__main__":
-    # args = parser.parse_args()
-    if len(argv) == 1:
-        print(help_mes)
-    else:
-        mode = argv[1]
-        if mode == 'int':
-            if argv[2].isnumeric() and argv[3].isnumeric():
-                int_gen_iter(int(argv[2]), int(argv[3]))
-            else:
-                print('Input error. Use -h to get more usage information')
-        elif mode == 'rep':
-            seq = argv[2].strip().split(sep=',')
-            if len(argv) > 3 and argv[3].isnumeric():
-                repeat_gen_iter(seq, int(argv[3]))
-            else:
-                repeat_gen_iter(seq)
-        elif mode == '-h':
+    try:
+        if len(argv) == 1:
             print(help_mes)
+        else:
+            mode = argv[1]
+            if mode == 'int':
+                if argv[2].isnumeric() and argv[3].isnumeric():
+                    int_gen_iter(int(argv[2]), int(argv[3]))
+                else:
+                    print('Input error. Use -h to get more usage information')
+            elif mode == 'rep':
+                seq = argv[2].strip().split(sep=',')
+                if len(argv) > 3 and argv[3].isnumeric():
+                    repeat_gen_iter(seq, int(argv[3]))
+                else:
+                    repeat_gen_iter(seq)
+            elif mode in ['-h', '-help']:
+                print(help_mes)
+    except Exception as ex:
+        print(ex)
 
 
